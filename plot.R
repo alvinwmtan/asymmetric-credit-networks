2fd80bbcedb5ab952743bacc6cec3fd5e1b131d3
alvinwmtan <alvin.tan1996@gmail.com>
2023-12-07 08:28:04 -0800

    added additional simulation and evaluation code

--- plot.R
+++ plot.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(here)
+library(Metrics)
 
 all_longs <- read_csv(here("all_longs.csv"))
 
@@ -14,3 +15,34 @@ p <- ggplot(all_longs, aes(x = iter, y = value, col = variable)) +
 
 ggsave("cum_plot.png", p, device = "png", 
        width = 12, height = 8, units = "cm")
+
+final_distribs <- read_csv(here("final_distribs.csv"))
+
+# rmse <- final_distribs |> 
+#   group_by(sim) |> 
+#   summarise(rmse = rmse(value, estimated))
+
+ggplot(rmse, aes(x = rmse)) +
+  geom_histogram()
+
+# entropy <- function(distrib) {
+#   -1 * sum(distrib * log2(distrib))
+# }
+# 
+# entropies <- final_distribs |> 
+#   group_by(sim) |> 
+#   summarise(obs_ent = entropy(value),
+#             est_ent = entropy(estimated))
+
+ggplot(entropies, aes(x = obs_ent, y = est_ent)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  geom_abline(slope = 1, intercept = 0,
+              col = "grey", lty = "dashed") +
+  theme_classic()
+
+sim_metrics <- read_csv(here("sim_metrics.csv"))
+
+ggplot(sim_metrics, aes(x = value, y = symmetry)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = "y ~ exp(x)")
